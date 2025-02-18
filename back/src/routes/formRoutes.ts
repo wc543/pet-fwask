3c5129fb6b28d58c2f5b8d29a7f44308567a02c5
wc543 <wc212223@gmail.com>
2025-02-18 12:36:28 -0500

    Setup express routers

---
+++ back/src/routes/formRoutes.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from 'express';
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('test form route');
+});
+
+export default router;
---
+++ back/src/routes/userRoutes.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from 'express';
+const router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('test user route');
+});
+
+export default router;
