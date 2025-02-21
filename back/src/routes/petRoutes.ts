8a9dbecadb360528c3693a3322a5fbb991da6983
km3949 <99452281+km3949@users.noreply.github.com>
2025-02-20 22:15:51 -0500

    get pet by id route

--- back/src/routes/petRoutes.ts
+++ back/src/routes/petRoutes.ts
@@ -17,15 +17,12 @@ async function getPets(req: Request, res: Response) {
 async function getPetsByUser(req: Request, res: Response) {
   const { username } = req.params;
   try {
-    let user = await db.get(`SELECT * FROM Users WHERE username = ?`, [username]);
+    const user = await db.get(`SELECT * FROM Users WHERE username = ?`, [username]);
     if (!user) {
       res.status(400).json({ error: "Username does not exist" });
     }
-    console.log("user", user);
     const user_id = user.user_id;
-    console.log("id", user_id);
     const pets = await db.all(`SELECT * FROM Pets WHERE created_by_id = ?`, [user_id]);
-    console.log("pets", pets);
     res.json(pets);
     if (!pets) {
       res.status(404).json({ message: "No pets created by this user" });
@@ -35,7 +32,22 @@ async function getPetsByUser(req: Request, res: Response) {
   }
 };
 
+// Get pet by id
+async function getPetById(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const pet = await db.get("SELECT * FROM Pets WHERE pet_id = ?", [id]);
+    if (!pet) {
+      res.status(400).json({ error: "Pet does not exist" });
+    }
+    res.json(pet);
+  } catch (error) {
+    res.status(404).json({ error: "Failed to fetch pet by id" });
+  }
+};
+
 router.get('/', getPets);
 router.get('/user/:username', getPetsByUser);
+router.get('/id/:id', getPetById);
 
 export default router;
