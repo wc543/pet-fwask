5ccc13c32be52dc591295f9f402192a52c9046a1
floooooo16 <fhh24@drexel.edu>
2025-03-14 11:40:42 -0400

    more nav styling

--- front/src/components/NavBar/NavBar.tsx
+++ front/src/components/NavBar/NavBar.tsx
@@ -2,6 +2,8 @@ import { Link, Outlet } from 'react-router-dom'
 import './NavBar.css'
 import { useContext } from 'react';
 import { AuthContext } from '../AuthContext';
+import { Typography } from '@mui/material';
+import PetsIcon from '@mui/icons-material/Pets';
 
 function Header() {
     const auth = useContext(AuthContext);
@@ -12,68 +14,72 @@ function Header() {
       if (role === 'STAFF') {
         navLinks = (
           <>
-            <Link to="/dashboard" className="nav-link">Dashboard</Link>
-            <Link to="/pets" className="nav-link">Find A Pet🐾</Link>
-            <Link to="/conversation-history" className="nav-link">View Messages</Link>
-            <Link to={`/forms/${auth.user.user_id}`}className="nav-link">Your Forms</Link>
-            <Link to="/logout" className="nav-link">Logout</Link>
+            <Link to="/dashboard" className="nav-link"><Typography variant='body1'>Dashboard</Typography></Link>
+            <Link to="/pets" className="nav-link"><Typography variant='body1'>Find A Pet🐾</Typography></Link>
+            <Link to="/conversation-history" className="nav-link"><Typography variant='body1'>Messages</Typography></Link>
+            <Link to={`/forms/${auth.user.user_id}`}className="nav-link"><Typography variant='body1'>Your Forms</Typography></Link>
+            <Link to="/logout" className="nav-link"><Typography variant='body1'>Logout</Typography></Link>
           </>
         );
       } else if (role === 'ADOPTER') {
         navLinks = (
           <>
             <Link to="/pets" className="nav-link">Find a Pet 🐾</Link>
-            <Link to="/conversation-history" className="nav-link">View Messages</Link>
-            <Link to="/forms" className="nav-link">Your Forms</Link>
-            <Link to={`/forms/SubmitFosterParentForm/${auth.user.user_id}`}className="nav-link">Become a Foster Parent</Link>
-            <Link to="/logout" className="nav-link">Logout</Link>
+            <Link to="/conversation-history" className="nav-link"><Typography variant='body1'>Messages</Typography></Link>
+            <Link to="/forms" className="nav-link"><Typography variant='body1'>Your Forms</Typography></Link>
+            <Link to={`/forms/SubmitFosterParentForm/${auth.user.user_id}`}className="nav-link"><Typography variant='body1'>Become a Foster Parent</Typography></Link>
+            <Link to="/logout" className="nav-link"><Typography variant='body1'>Logout</Typography></Link>
           </>
         );
       } else if (role === 'FOSTER') {
         navLinks = (
           <>
-            <Link to="/pets" className="nav-link" >Find A Pet 🐾</Link>
-            <Link to="/conversation-history" className="nav-link">View Messages</Link>
-            <Link to={`/forms/${auth.user.user_id}`} className="nav-link">Your Forms</Link>
-            <Link to="/logout" className="nav-link">Logout</Link>
+            <Link to="/pets" className="nav-link" ><Typography variant='body1'>Find A Pet🐾</Typography></Link>
+            <Link to="/conversation-history" className="nav-link"><Typography variant='body1'>Messages</Typography></Link>
+            <Link to={`/forms/${auth.user.user_id}`} className="nav-link"><Typography variant='body1'>Your Forms</Typography></Link>
+            <Link to="/logout" className="nav-link"><Typography variant='body1'>Logout</Typography></Link>
           </>
         );
       }
     } else {
       navLinks = (
         <>
-          <Link to="/pets" className="nav-link">Find A Pet 🐾</Link>
+          <Link to="/pets" className="nav-link"><Typography variant='body1'>Find A Pet🐾</Typography></Link>
           {/* For now, "Apply to be a Foster" simply redirects to the login page */}
-          <Link to="/login" className="nav-link">Become a Foster Parent</Link>
-          <Link to="/login" className="nav-link">Login</Link>
-          <Link to="/signup" className="nav-link">Sign Up</Link>
+          <Link to="/login" className="nav-link"><Typography variant='body1'>Become a Foster Parent</Typography></Link>
+          <Link to="/login" className="nav-link"><Typography variant='body1'>Login</Typography></Link>
+          <Link to="/signup" className="nav-link"><Typography variant='body1'>Sign up</Typography></Link>
         </>
       );
     }
-  
+
+    function Logo(){
+      return(
+        <>
+          <div className="nav-logo">
+            <Typography variant='h5' color='success'>Petworking</Typography>
+            <PetsIcon fontSize={'large'}  className='.nav-logo-item'/>
+          </div>
+
+        </>
+      )
+    }
+
     return (
       <div className="navbar">
+        <Logo/>
         {navLinks}
         {auth?.user && (
-          <span className="nav-user">Welcome, {auth.user.username}!</span>
+          <span className="nav-user"><Typography variant='body1'>Welcome, {auth.user.username}!</Typography></span>
         )}
       </div>
     );
-}
-
-function Logo(){
-  return(
-    <>
-      <img ></img>
-    </>
-  )
 }
 
 function NavBar() {
     return (
         <>
             <nav>
-                <Logo/>
                 <Header />
             </nav>
             <main>
